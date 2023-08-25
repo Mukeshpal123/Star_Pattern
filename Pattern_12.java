22c7038a8aaf09e2c70e3fc51958bbbff948ab60
Mukesh Kumar Pal <mukeshmech294@gmail.com>
2023-08-25 06:49:28 +0530

    patterns

---
+++ Pattern_12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                if (j%2==0){
+                    System.out.print("!\t");
+                }
+                else{
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
